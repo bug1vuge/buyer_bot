f49aecf4a6dc625fdcab99abed558cb9a7bc6042
Максим <81079097+bug1vuge@users.noreply.github.com>
2025-12-16 03:58:14 +0700

    Update tinkoff_client.py

--- backend/app/tinkoff_client.py
+++ backend/app/tinkoff_client.py
@@ -117,7 +117,7 @@ def build_paid_message(order, product) -> str:
 def send_admin_notification(text: str):
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
-        "chat_id": settings.TELEGRAM_NOTIFY_CHAT_ID,
+        "chat_id": settings.ADMIN_CHAT_ID,
         "text": text
     }
 
