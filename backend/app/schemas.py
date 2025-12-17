5984683187b11bb0e2c95699a78a370168a8b782
Максим <81079097+bug1vuge@users.noreply.github.com>
2025-12-17 11:56:02 +0700

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, Union, List
+from datetime import date
 
 class CreateOrderIn(BaseModel):
     product_id: int
@@ -14,3 +15,22 @@ class CreateOrderIn(BaseModel):
 class CreateOrderOut(BaseModel):
     order_id: str
     confirmation_url: str
+
+class SalesReportIn(BaseModel):
+    period: Optional[Union[int, str]] = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+
+class SalesReportItem(BaseModel):
+    product_title: str
+    quantity: int
+    total_amount: int
+    agent_fee: int
+
+class SalesReportOut(BaseModel):
+    period_from: date
+    period_to: date
+    items: List[SalesReportItem]
+    total_sum: int
+    total_agent_fee: int
+
