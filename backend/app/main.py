138b91ba8e197be2a72467c4ca9565a750e79b5f
Максим <81079097+bug1vuge@users.noreply.github.com>
2025-12-19 11:07:11 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
 from .config import settings
 from .models import Base, Product, Order
-from .schemas import CreateOrderIn, CreateOrderOut, SalesReportIn, SalesReportItem, SalesReportOut
+from .schemas import CreateOrderIn, CreateOrderOut, SalesReportIn, SalesReportItem, SalesReportOut, CancelOrderIn
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from reportlab.lib.pagesizes import A4
