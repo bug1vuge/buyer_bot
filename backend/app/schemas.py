de72eb1e427b8611b2d0dd1ddbfdc36aec2a199a
Максим <81079097+bug1vuge@users.noreply.github.com>
2025-12-20 14:10:59 +0700

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -41,3 +41,8 @@ class DeleteSalesReportIn(BaseModel):
     period: int | str | None = None
     start_date: date | None = None
     end_date: date | None = None
+
+class DeleteClientsReportIn(BaseModel):
+    period: int | str | None = None
+    start_date: date | None = None
+    end_date: date | None = None
